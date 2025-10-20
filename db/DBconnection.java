a70f46e948a60ee9094b46ab6fa74a4b327cdb4d
Bharath Krishna <phantomkrisx@gmail.com>
2025-10-20 17:25:10 +0530

    fixed bugs

--- db/DBconnection.java
+++ db/DBconnection.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBConnection {
+public class DBconnection {
     private static final String URL = "jdbc:mysql://localhost:3306/complaint_system";
     private static final String USERNAME = "root"; 
     private static final String PASSWORD = ""; 
